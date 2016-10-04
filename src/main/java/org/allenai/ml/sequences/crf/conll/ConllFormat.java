d1db2bf45177ec61a17f9c68b2a541b2b9ed3906
Dirk Groeneveld <dirkg@allenai.org>
2016-10-04 13:49:32 -0700

    Imports we need to make this compile

--- src/main/java/org/allenai/ml/sequences/crf/conll/ConllFormat.java
+++ src/main/java/org/allenai/ml/sequences/crf/conll/ConllFormat.java
@@ -23,6 +23,8 @@ import lombok.val;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
